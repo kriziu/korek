83c695d4abc929a14d2ee22e584fc65238780ea9
kriziu <brunodzi07@gmail.com>
2021-10-12 00:41:50 +0200

    security update

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import userController from './controllers/userController';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+const { DB_PASS } = process.env;
+
 // DATABASE
 mongoose.connect(
-  `mongodb+srv://admin:${'4hGgszYmxZqjvOD1'}@cluster0.wqs9m.mongodb.net/Cluster0?retryWrites=true&w=majority`,
+  `mongodb+srv://admin:${DB_PASS}@cluster0.wqs9m.mongodb.net/Cluster0?retryWrites=true&w=majority`,
   err => {
     if (!err) console.log('Connection succes');
     else console.log('Error in connection: ' + err);
